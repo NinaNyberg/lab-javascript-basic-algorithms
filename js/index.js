11ee79a4d84b976e151bb5c8c85cf4e08233ff20
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-01-15 18:13:57 +0200

    Two and half tasks done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
-// Iteration 1: Names and Input
+let hacker1 = "Jim"
+console.log(`The drivers name is ${hacker1}`)
+let hacker2 = "John"
+console.log(`The navigators name is ${hacker2}`)
 
-// Iteration 2: Conditionals
+let capNameDriver = hacker1.toUpperCase().split("")
+console.log(capNameDriver)
+
+/*
+if (hacker1.localeCompare(hacker2) === 1) {
+  return `The driver has the longest name, it has ${hacker1} characters`
+} else if (hacker1.localeCompare(hacker2) === -1) {
+  return `It seems that the navigator has the longest name, it has ${hacker2} characters`
+} else {
+  return `Wow, they both have equally long names, ${hacker1} characters`
+}
+*/
 
 
-// Iteration 3: Loops
+
